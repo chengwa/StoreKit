0e95f83a7eeb543da6d9252f717427adc3454aff
closure <retval@me.com>
2015-03-21 12:27:39 +0800

    add cache system to bucket

--- StoreKit/Core/RSBucket.h
+++ StoreKit/Core/RSBucket.h
@@ -10,11 +10,52 @@
 
 @class RSStorage;
 
+
+typedef NS_ENUM(NSInteger, RSBucketCacheType) {
+    RSBucketCacheTypeNone,
+    RSBucketCacheTypeDisk,
+    RSBucketCacheTypeMemory
+};
+
+typedef void(^RSBucketNoParamsBlock)();
+
+typedef void(^RSBucketQueryCompletedBlock)(NSData *data, RSBucketCacheType cacheType);
+
+typedef void(^RSBucketCheckCacheCompletionBlock)(BOOL isInCache);
+
+typedef void(^RSBucketCalculateSizeBlock)(NSUInteger fileCount, NSUInteger totalSize);
+
+typedef NSString *(^RSBucketCacheKeyFilterBlock)(NSURL *url);
+
 @interface RSBucket : RSObject
 @property (strong, nonatomic, readonly) NSString *path;
 @property (strong, nonatomic, readonly) NSString *name;
 - (instancetype)initWithStorage:(RSStorage *)storage name:(NSString *)name;
-
-- (BOOL)setObject:(id <NSCoding>)object forKey:(id<RSPrimaryKey>)aKey;
-- (id <NSCoding>)objectForKey:(id <RSPrimaryKey>)key;
 @end
+
+@interface RSBucket (Cache)
+- (void)addReadOnlyCachePath:(NSString *)path;
+- (void)storeData:(NSData *)data forKey:(NSString *)key;
+- (void)storeData:(NSData *)data forKey:(NSString *)key toDisk:(BOOL)toDisk;
+- (NSOperation *)queryDiskCacheForKey:(NSString *)key done:(RSBucketQueryCompletedBlock)doneBlock;
+- (NSData *)dataFromMemoryCacheForKey:(NSString *)key;
+- (NSData *)dataFromDiskCacheForKey:(NSString *)key;
+- (void)removeDataForKey:(NSString *)key;
+- (void)removeDataForKey:(NSString *)key withCompletion:(RSBucketNoParamsBlock)completion;
+- (void)removeDataForKey:(NSString *)key fromDisk:(BOOL)fromDisk;
+- (void)removeDataForKey:(NSString *)key fromDisk:(BOOL)fromDisk withCompletion:(RSBucketNoParamsBlock)completion;
+- (void)clearMemory;
+- (void)clearDiskOnCompletion:(RSBucketNoParamsBlock)completion;
+- (void)clearDisk;
+- (void)cleanDiskWithCompletionBlock:(RSBucketNoParamsBlock)completionBlock;
+- (void)cleanDisk;
+- (NSUInteger)getSize;
+- (NSUInteger)getDiskCount;
+- (void)calculateSizeWithCompletionBlock:(RSBucketCalculateSizeBlock)completionBlock;
+- (void)diskDataExistsWithKey:(NSString *)key completion:(RSBucketCheckCacheCompletionBlock)completionBlock;
+- (BOOL)diskDataExistsWithKey:(NSString *)key;
+- (NSString *)cachePathForKey:(NSString *)key inPath:(NSString *)path;
+- (NSString *)defaultCachePathForKey:(NSString *)key;
+@end
+
+
