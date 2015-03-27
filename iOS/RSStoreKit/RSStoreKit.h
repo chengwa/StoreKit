b2f564861ab20723b6bd509cbb4f406ce68285ca
closure <retval@me.com>
2015-03-27 17:06:45 +0800

    add settings

---
+++ iOS/RSStoreKit/RSStoreKit.h
@@ -0,0 +1,19 @@
+//
+//  RSStoreKit.h
+//  RSStoreKit
+//
+//  Created by closure on 3/26/15.
+//  Copyright (c) 2015 closure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RSStoreKit.
+FOUNDATION_EXPORT double RSStoreKitVersionNumber;
+
+//! Project version string for RSStoreKit.
+FOUNDATION_EXPORT const unsigned char RSStoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSStoreKit/PublicHeader.h>
+
+
